8b08889a192c180499be7494c2b12e76b5367bd7
Alexey Dobriyan <adobriyan@gmail.com>
2018-02-06 15:36:55 -0800

    proc: don't use READ_ONCE/WRITE_ONCE for /proc/*/fail-nth
    READ_ONCE and WRITE_ONCE are useless when there is only one read/write
    is being made.
    
    Link: http://lkml.kernel.org/r/20171120204033.GA9446@avx2
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Akinobu Mita <akinobu.mita@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 9f7118b2007d5e7c7a061550d2ca2ecb841537dc)
    (cherry picked from commit 109265eb17afbf9c7707a53ea73cb1731466ded8)

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -1492,7 +1492,7 @@ static ssize_t proc_fail_nth_write(struct file *file, const char __user *buf,
 	task = get_proc_task(file_inode(file));
 	if (!task)
 		return -ESRCH;
-	WRITE_ONCE(task->fail_nth, n);
+	task->fail_nth = n;
 	put_task_struct(task);
 
 	return count;
@@ -1508,8 +1508,7 @@ static ssize_t proc_fail_nth_read(struct file *file, char __user *buf,
 	task = get_proc_task(file_inode(file));
 	if (!task)
 		return -ESRCH;
-	len = snprintf(numbuf, sizeof(numbuf), "%u\n",
-			READ_ONCE(task->fail_nth));
+	len = snprintf(numbuf, sizeof(numbuf), "%u\n", task->fail_nth);
 	len = simple_read_from_buffer(buf, count, ppos, numbuf, len);
 	put_task_struct(task);
 
