f28e390061f78f988a392a1126358e4b6b49129b
Al Viro <viro@zeniv.linux.org.uk>
2018-05-03 09:21:05 -0400

    procfs: switch instantiate_t to d_splice_alias()
    ... and get rid of pointless struct inode *dir argument of those,
    while we are at it.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    (cherry picked from commit 0168b9e38c4213e162b15444c071d4198ff470e8)
    (cherry picked from commit fa753d5b99bb256e32dd689c91d6757bff9824cf)

--- fs/proc/internal.h
+++ fs/proc/internal.h
@@ -177,7 +177,7 @@ extern  unsigned int uclamp_task_util(struct task_struct *p,
 					unsigned int clamp_id);
 
 /* Lookups */
-typedef int instantiate_t(struct inode *, struct dentry *,
+typedef struct dentry *instantiate_t(struct dentry *,
 				     struct task_struct *, const void *);
 extern bool proc_fill_cache(struct file *, struct dir_context *, const char *, int,
 			   instantiate_t, struct task_struct *, const void *);
