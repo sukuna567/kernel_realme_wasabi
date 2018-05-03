1d68fdd81cd1527357f4a2462228bff5adfbf453
Al Viro <viro@zeniv.linux.org.uk>
2018-05-03 09:45:06 -0400

    switch the rest of procfs lookups to d_splice_alias()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    (cherry picked from commit 888e2b03ef56694290e58bd9ac23f8033bf6369f)
    (cherry picked from commit da258ef76aa9f3a6b5b0ad97129e9a26a881bd11)

--- fs/proc/proc_sysctl.c
+++ fs/proc/proc_sysctl.c
@@ -560,9 +560,8 @@ static struct dentry *proc_sys_lookup(struct inode *dir, struct dentry *dentry,
 		goto out;
 	}
 
-	err = NULL;
 	d_set_d_op(dentry, &proc_sys_dentry_operations);
-	d_add(dentry, inode);
+	err = d_splice_alias(inode, dentry);
 
 out:
 	if (h)
@@ -718,6 +717,7 @@ static bool proc_sys_fill_cache(struct file *file,
 		if (IS_ERR(child))
 			return false;
 		if (d_in_lookup(child)) {
+			struct dentry *res;
 			inode = proc_sys_make_inode(dir->d_sb, head, table);
 			if (IS_ERR(inode)) {
 				d_lookup_done(child);
@@ -725,7 +725,16 @@ static bool proc_sys_fill_cache(struct file *file,
 				return false;
 			}
 			d_set_d_op(child, &proc_sys_dentry_operations);
-			d_add(child, inode);
+			res = d_splice_alias(inode, child);
+			d_lookup_done(child);
+			if (unlikely(res)) {
+				if (IS_ERR(res)) {
+					dput(child);
+					return false;
+				}
+				dput(child);
+				child = res;
+			}
 		}
 	}
 	inode = d_inode(child);
